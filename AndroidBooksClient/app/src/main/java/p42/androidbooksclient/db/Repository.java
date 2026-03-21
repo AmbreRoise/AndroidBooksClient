13e9133fbc4762e2a287f449694be1b77f11f610
Elora Fouilleul <efouilleul@unistra.fr>
2026-03-21 08:41:27 +0100

    Retrofit : ajout requête de suppression d'un livre

--- AndroidBooksClient/app/src/main/java/p42/androidbooksclient/db/Repository.java
+++ AndroidBooksClient/app/src/main/java/p42/androidbooksclient/db/Repository.java
@@ -229,7 +229,11 @@ public class Repository {
         bookService.deleteOneBook(bookID).enqueue(new Callback<ResponseBody>() {
             @Override
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-
+                if(response.isSuccessful()){
+                    Log.i("Repository", "Book " + bookID + " deleted");
+                } else {
+                    Log.e("Repository", "deleteBook failed with status: " + response.code());
+                }
             }
 
             @Override
